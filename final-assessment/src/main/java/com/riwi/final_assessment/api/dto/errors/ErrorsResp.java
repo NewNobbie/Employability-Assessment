2051450255d5310aa87b23e14990335a5a91b410
jhamile08 <jhamiledr222@gmail.com>
2024-07-29 09:02:43 -0500

    task-001/feat: hexagonal structure

---
+++ final-assessment/src/main/java/com/riwi/final_assessment/api/dto/errors/ErrorsResp.java
@@ -0,0 +1,18 @@
+package com.riwi.final_assessment.api.dto.errors;
+import java.util.List;
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorsResp extends BaseErrorResp{
+    private List<Map<String,String>> errors; 
+}
