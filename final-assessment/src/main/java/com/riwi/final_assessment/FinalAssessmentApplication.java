6e66d22100f863b134e4c854846f57f72e0debfd
NewNobbie <116767117+NewNobbie@users.noreply.github.com>
2024-07-29 08:34:48 -0500

    Sprint Initialzr
    Dependencies:
    - JPA
    - MySQL Driver 
    - Lombok
    - DevTools
    - Validation
    - Spring Web 
    - Docker Compose Support

---
+++ final-assessment/src/main/java/com/riwi/final_assessment/FinalAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.final_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalAssessmentApplication.class, args);
+	}
+
+}
---
+++ final-assessment/src/test/java/com/riwi/final_assessment/FinalAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.final_assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
