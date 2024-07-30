00991148f9b6fc33324beb3e236632eda36c8283
jhamile08 <jhamiledr222@gmail.com>
2024-07-30 12:47:51 -0500

    task-007/feat: create coupon

--- final-assessment/src/main/java/com/riwi/final_assessment/infrastructure/services/CouponHistoryService.java
+++ final-assessment/src/main/java/com/riwi/final_assessment/infrastructure/services/CouponHistoryService.java
@@ -1,15 +1,28 @@
 package com.riwi.final_assessment.infrastructure.services;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.riwi.final_assessment.api.dto.request.CouponHistoryRequest;
 import com.riwi.final_assessment.api.dto.response.CouponBasicResponse;
+import com.riwi.final_assessment.domain.entities.Coupon;
+import com.riwi.final_assessment.domain.entities.CouponHistory;
+import com.riwi.final_assessment.domain.repositories.CouponHistoryRepository;
+import com.riwi.final_assessment.domain.repositories.CouponRepository;
 import com.riwi.final_assessment.infrastructure.abstract_service.ICouponHistoryService;
 
 public class CouponHistoryService implements ICouponHistoryService{
 
+    @Autowired
+    private CouponHistoryRepository couponHistoryRepository;
+    @Autowired
+    private ModelMapper modelMapper;
+
     @Override
     public CouponBasicResponse create(CouponHistoryRequest rq) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'create'");
+        CouponHistory couponHistory = modelMapper.map(rq, CouponHistory.class);
+        CouponHistory couponHistorySaved = couponHistoryRepository.save(couponHistory);
+        return modelMapper.map(couponHistorySaved, CouponBasicResponse.class);
     }
     
 }
